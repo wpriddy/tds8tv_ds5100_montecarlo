e11dbff8c91f1a8b0be8ab54907710180519d123
Wyatt Priddy <wyattpriddy50@gmail.com>
2023-12-02 08:29:20 -0500

    editing params

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(name='montecarlo',
       url='https://github.com/wpriddy/tds8tv_ds5100_montecarlo',
       author_email='wpriddy@tds8tv.edu',
       license='MIT',
+      packages=['montecarlo'],
       # Only non-standard libraries
-      installs_required=['numpy>=1.19.2', 
+      install_requires=['numpy>=1.19.2', 
                          'pandas>=1.1.3',
-                         '']
+                         'matplotlib>=3.8.2']
      )
